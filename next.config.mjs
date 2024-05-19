00906bb2c6226110f630fb65234fb4d27e2a4da2
Yi Pei Zhu <studentypz@gmail.com>
2024-05-19 22:14:22 +0200

    revisit nextjs - dynamic route

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "zbxzxsfmhicrxyicaqzk.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabinImages/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/cabins/[cabinId]/page.tsx
+++ src/app/cabins/[cabinId]/page.tsx
@@ -1,24 +1,25 @@
-import { CabinType } from "@/lib/types";
+import { getCabin } from "@/lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
-// PLACEHOLDER DATA
-const cabin = {
-  id: 89,
-  name: "001",
-  maxCapacity: 2,
-  regularPrice: 250,
-  discount: 0,
-  description:
-    "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
-  image:
-    "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
-};
-type CabinProps = {
-  cabin: CabinType;
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: { cabinId: number };
+}) {
+  const { name } = await getCabin(params.cabinId);
+  return {
+    title: `Cabin ${name}`,
+  };
+}
 
-export default function Cabin({ cabin }: CabinProps) {
+export default async function CabinDetail({
+  params,
+}: {
+  params: { cabinId: number };
+}) {
+  const cabin = await getCabin(params.cabinId);
+  if (!cabin) return null;
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
 
@@ -26,7 +27,12 @@ export default function Cabin({ cabin }: CabinProps) {
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
         <div className="relative scale-[1.15] -translate-x-3">
-          <Image src={image} alt={`Cabin ${name}`} />
+          <Image
+            fill
+            className="object-cover"
+            src={image}
+            alt={`Cabin ${name}`}
+          />
         </div>
 
         <div>
